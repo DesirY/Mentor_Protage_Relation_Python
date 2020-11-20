85054bedd89f120ddc99de9b94d4cf21a8f833c5
XinyuanYan <1779929499@qq.com>
2020-11-20 18:03:22 +0800

    the first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import csv
+import json
+
+def processCsv():
+    '''
+    处理csv文件，统计一共有多少条属性为1和2的边，以及这些边涉及到多少个节点
+    输出关于节点和边的json文件
+    :return:
+    '''
+
+    # 输出项
+    res = {}
+    nodesLst = []
+    edgesLst = []
+    nodeId = []
+
+    res2 = {}
+    nodesLst2 = []
+    edgesLst2 = []
+    nodeId2 = []
+
+    with open("./data/mentor_protege_relation_univ_time_NeuroMath.csv", 'r') as f:
+        f_csv = csv.reader(f)
+        next(f_csv)
+        count = 0
+
+        for row in f_csv:
+            edgeType = int(row[6])
+
+            if edgeType == 2:
+                sourceId = int(row[0])
+                targetId = int(row[2])
+                edge = {"source": sourceId, "target": targetId, "type": edgeType}
+                edgesLst.append(edge)
+
+                if sourceId not in nodeId:
+                    nodeId.append(sourceId)
+                    sourceNode = {"id": sourceId}
+                    nodesLst.append(sourceNode)
+
+                if targetId not in nodeId:
+                    nodeId.append(targetId)
+                    targetNode = {"id": targetId}
+                    nodesLst.append(targetNode)
+
+            elif edgeType == 3:
+                sourceId = int(row[0])
+                targetId = int(row[2])
+                edge = {"source": sourceId, "target": targetId, "type": edgeType}
+                edgesLst2.append(edge)
+
+                if sourceId not in nodeId2:
+                    nodeId2.append(sourceId)
+                    sourceNode = {"id": sourceId}
+                    nodesLst2.append(sourceNode)
+
+                if targetId not in nodeId2:
+                    nodeId2.append(targetId)
+                    targetNode = {"id": targetId}
+                    nodesLst2.append(targetNode)
+
+
+
+
+            count += 1
+
+            print(count)
+
+        res["nodes"] = nodesLst
+        res["edges"] = edgesLst
+
+        res2["nodes"] = nodesLst2
+        res2["edges"] = edgesLst2
+
+        print("节点的个数", len(nodesLst))
+        print("边的个数", len(edgesLst))
+        print("节点的个数2", len(nodesLst2))
+        print("边的个数2", len(edgesLst2))
+
+    with open("relation_2.json", "w") as f:
+        json.dump(res, f, indent=2)
+
+    with open("relation_3.json", "w") as f:
+        json.dump(res2, f, indent=2)
+
+if __name__ == "__main__":
+    processCsv()
